708e19923f9d067d3415689357f61a637334b4a7
Louie <causingljbcit@gmail.com>
2022-11-15 09:46:37 -0800

    Naming functions 3 ways

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// input = prompt("Welcome! What is your Name?")
+// greet(input)
+
+// function greet(string){
+//     console.log(`Hello ${string}`)
+// }
+
+// let greet = function (string) {
+//     console.log(`Hello ${string}`);
+// };
+
+
+let greet = userName => {
+    console.log(`Hello ${userName}`)
+}
+
+greet("Louie");
+
